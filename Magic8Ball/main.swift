3f1389e550dff766a07de61da1f2591e82c5b0a2
Benjamn <bfreeman@lcs.on.ca>
2021-09-16 21:31:36 -0400

    I added a switch and a Loop to create the processing part of the 8 Ball

--- Magic8Ball/main.swift
+++ Magic8Ball/main.swift
@@ -9,18 +9,26 @@ import Foundation
 // Program name
 print("MAGIC 8 BALL")
 print("============")
-
+var loop = true
 // Ask for user's question
 print("What is your question? ")
+while loop {
 // Get input as a non-optional String
-let input = readLine()!
-
-// Print out their question
-print("You said: \(input)")
+    _ = readLine()!
 
 // Select a random value
+
 let value = Int.random(in: 1...8)
+switch value {
+case 1: print("Yes")
+case 2: print("No")
+case 3: print("The answer is uncertain.")
+case 4: print("The answer is 'Likly'.")
+case 5: print("Only time will tell.")
+case 6: print("One day.")
+case 7: print("Try again.")
+case 8: print("The ball is tierd, attempt again later.")
+default: print("No Answer")
+}
 
-// Print the random value
-print("The value is: \(value)")
-
+}

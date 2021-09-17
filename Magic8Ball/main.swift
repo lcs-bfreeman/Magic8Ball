93894bb0be7da2692c4432e5adaad867b1533d60
Benjamn <bfreeman@lcs.on.ca>
2021-09-17 11:21:40 -0400

    Got the prompt to repeat

--- Magic8Ball/main.swift
+++ Magic8Ball/main.swift
@@ -11,24 +11,26 @@ print("MAGIC 8 BALL")
 print("============")
 var loop = true
 // Ask for user's question
-print("What is your question? ")
 while loop {
-// Get input as a non-optional String
+    
+    print("What is your question? ")
+
+    // Get input as a non-optional String
     _ = readLine()!
-
-// Select a random value
-
-let value = Int.random(in: 1...8)
-switch value {
-case 1: print("Yes")
-case 2: print("No")
-case 3: print("The answer is uncertain.")
-case 4: print("The answer is 'Likly'.")
-case 5: print("Only time will tell.")
-case 6: print("One day.")
-case 7: print("Try again.")
-case 8: print("The ball is tierd, attempt again later.")
-default: print("No Answer")
-}
-
+    
+    // Select a random value
+    
+    let value = Int.random(in: 1...8)
+    switch value {
+    case 1: print("Yes")
+    case 2: print("No")
+    case 3: print("The answer is uncertain.")
+    case 4: print("The answer is 'Likly'.")
+    case 5: print("Only time will tell.")
+    case 6: print("One day.")
+    case 7: print("Try again.")
+    case 8: print("The ball is tierd, attempt again later.")
+    default: print("No Answer")
+    }
+    
 }
